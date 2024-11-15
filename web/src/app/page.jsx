a5083b86a8206a4f5d80983f88bfc2cf788f8c45
fede <fblancocavallero@gmail.com>
2024-11-15 11:23:43 -0300

    cretion

--- web/src/app/page.jsx
+++ web/src/app/page.jsx
@@ -2,7 +2,7 @@ import { SectionA } from "./components/sections/sectionA"
 import { SectionB } from "./components/sections/sectionB"
 import { SectionC } from "./components/sections/sectionC"
 import { SectionD } from "./components/sections/sectionD"
-
+import { SectionE } from "./components/sections/sectionE"
 
 export default function Landing() {
   return (
@@ -12,6 +12,7 @@ export default function Landing() {
         <SectionB />
         <SectionC />
         <SectionD />
+        <SectionE />
       </main>
     </>
   )
