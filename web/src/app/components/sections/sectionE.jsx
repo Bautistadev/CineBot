83502c7905ea3130fc951c45b3080fd58c004f83
fede <fblancocavallero@gmail.com>
2024-11-16 07:35:54 -0300

    mapa anda faltan markers

--- web/src/app/components/sections/sectionE.jsx
+++ web/src/app/components/sections/sectionE.jsx
@@ -2,17 +2,24 @@
 
 import { useEffect } from "react";
 
+const mapKey = process.env.NEXT_PUBLIC_MAP_KEY;
+
 export const SectionE = () => {
     useEffect(() => {
-        // Cargar la API de Google Maps dinámicamente
+        if (!mapKey) {
+            console.error("Google Maps API key is missing!");
+            return;
+        }
+
         const loadGoogleMapsScript = () => {
             return new Promise((resolve, reject) => {
                 if (window.google) {
                     resolve(); // La API ya está cargada
                     return;
                 }
+
                 const script = document.createElement("script");
-                script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAPVA8gh-DB9n-Wy7OmdcmUtXo4nhxjLro`;
+                script.src = `https://maps.googleapis.com/maps/api/js?key=${mapKey}&libraries=marker`;
                 script.async = true;
                 script.defer = true;
                 script.onload = resolve; // Resuelve la promesa cuando se carga
@@ -23,7 +30,7 @@ export const SectionE = () => {
 
         const initMap = () => {
             const locations = [
-                { lat: -34.914506971122385, lng:  -57.95116686002905, title: "Cinema Rocha" },
+                { lat: -34.914506971122385, lng: -57.95116686002905, title: "Cinema Rocha" },
                 { lat: -34.916811941489954, lng: -57.95247577789763, title: "Cinema City" },
                 { lat: -34.915721047551486, lng: -57.956788769563, title: "Cinema Paraiso" },
                 { lat: -34.91679434654146, lng: -57.949321500411024, title: "Cinema Ocho" },
@@ -32,19 +39,18 @@ export const SectionE = () => {
 
             const map = new google.maps.Map(document.getElementById("map"), {
                 zoom: 16,
-                center: { lat: -34.914506971122385, lng:  -57.95116686002905 },
+                center: { lat: -34.914506971122385, lng: -57.95116686002905 },
             });
 
             locations.forEach((location) => {
-                new google.maps.Marker({
-                    position: { lat: location.lat, lng: location.lng },
+                new google.maps.marker.AdvancedMarkerElement({
                     map: map,
+                    position: { lat: location.lat, lng: location.lng },
                     title: location.title,
                 });
             });
         };
 
-        // Cargar el script de Google Maps y luego inicializar el mapa
         loadGoogleMapsScript()
             .then(() => {
                 initMap();
@@ -52,7 +58,7 @@ export const SectionE = () => {
             .catch((err) => {
                 console.error("Error al cargar la API de Google Maps:", err);
             });
-    }, []); // Ejecutar el efecto solo una vez al montar el componente
+    }, []);
 
     return (
         <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-200 dark:bg-gray-200">
@@ -61,7 +67,6 @@ export const SectionE = () => {
                     Locales Asociados
                 </h2>
                 <div className="mx-auto max-w-4xl space-y-4 bg-slate-600 rounded-lg p-4">
-                    {/* Contenedor del mapa */}
                     <div id="map" style={{ height: "500px", width: "100%" }}></div>
                 </div>
             </div>
